2f3a14b3535c328e6e83ca26f77ec076e9fb33f3
Cameron Dempsey <cdempsey9@gatech.edu>
2020-01-11 23:21:36 -0600

    Hello World!

---
+++ projects/Main/main.cpp
@@ -0,0 +1,14 @@
+/*
+    Platform
+    Cameron Dempsey
+    James Tubbs
+*/
+
+
+#include <iostream>
+
+int main(int argc, char* argv[])
+{
+    std::cout << "Hello World!" << std::endl;
+    return 0;
+}
